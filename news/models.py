87eaa3d660dd207a303a9af44ee59145406262fb
Ibrat <ibratanvarov@gmail.com>
2020-08-26 15:48:47 +0500

    project about uzbekistan

---
+++ news/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.urls import reverse
+
+class News(models.Model):
+
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    views = models.IntegerField(default=0)
+
+
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="name of category")
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
